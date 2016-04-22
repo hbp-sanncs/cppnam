d8969d4a070cb0bbaba429695ab427c098128a7a
Christoph Jenzen <cjenzen@techfak.uni-bielefeld.de>
2016-04-22 11:02:16 +0200

    tidy up

--- test/util/test_binary_matrix.cpp
+++ test/util/test_binary_matrix.cpp
@@ -16,12 +16,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "gtest/gtest.h"
-
 #include <cstdint>
-#include <limits>
-#include <iostream>
-
+#include <gtest/gtest.h>
 #include <util/binary_matrix.hpp>
 
 namespace nam {
@@ -36,11 +32,11 @@ TEST(BinaryMatrix, constexpressions)
 	EXPECT_EQ(2, bin.numberOfCells(15));
 	EXPECT_EQ(2, bin.numberOfCells(16));
 	EXPECT_EQ(3, bin.numberOfCells(17));
-	
-	EXPECT_NO_THROW(bin.check_range(2,8));
+
+	EXPECT_NO_THROW(bin.check_range(2, 8));
 	EXPECT_ANY_THROW(bin.check_range(3, 8));
 	EXPECT_ANY_THROW(bin.check_range(2, 9));
-	EXPECT_NO_THROW(bin.check_range_cells(2,1));
+	EXPECT_NO_THROW(bin.check_range_cells(2, 1));
 	EXPECT_ANY_THROW(bin.check_range_cells(3, 1));
 	EXPECT_ANY_THROW(bin.check_range_cells(2, 2));
 }
@@ -48,58 +44,57 @@ TEST(BinaryMatrix, constexpressions)
 TEST(BinaryMatrix, manipulation)
 {
 	BinaryMatrix<uint8_t> bin(3, 9);
-	bin.set_cell(0,0,1);
-	
-	EXPECT_EQ(1, bin.get_cell(0,0));
-	EXPECT_EQ(0, bin.get_cell(0,1));
-	EXPECT_ANY_THROW(bin.get_cell(0,2));
-	EXPECT_EQ(true, bin.get_bit(0,0));
-	EXPECT_EQ(false, bin.get_bit(0,1));
-	EXPECT_ANY_THROW(bin.get_bit(0,9));
-	
+	bin.set_cell(0, 0, 1);
+
+	EXPECT_EQ(1, bin.get_cell(0, 0));
+	EXPECT_EQ(0, bin.get_cell(0, 1));
+	EXPECT_ANY_THROW(bin.get_cell(0, 2));
+	EXPECT_EQ(true, bin.get_bit(0, 0));
+	EXPECT_EQ(false, bin.get_bit(0, 1));
+	EXPECT_ANY_THROW(bin.get_bit(0, 9));
+
 	EXPECT_TRUE(bin.row_vec(0).get_bit(0));
 	EXPECT_TRUE(bin.row_vec(0).get_cell(0));
 	EXPECT_FALSE(bin.row_vec(0).get_bit(1));
 	EXPECT_FALSE(bin.row_vec(0).get_cell(1));
-	
+
 	BinaryVector<uint8_t> vec(9);
 	vec.set_bit(1);
-	
+
 	EXPECT_EQ(2, vec.get_cell(0));
 	EXPECT_EQ(0, vec.get_cell(1));
 	EXPECT_ANY_THROW(vec.get_cell(2));
 	EXPECT_EQ(true, vec.get_bit(1));
 	EXPECT_EQ(false, vec.get_bit(0));
 	EXPECT_ANY_THROW(vec.get_bit(9));
-	
-	
-	bin.write_vec(1,vec);
-	
-	EXPECT_EQ(2, bin.get_cell(1,0));
-	EXPECT_EQ(0, bin.get_cell(1,1));
-	EXPECT_EQ(true, bin.get_bit(1,1));
-	EXPECT_EQ(false, bin.get_bit(1,0));
-	
+
+	bin.write_vec(1, vec);
+
+	EXPECT_EQ(2, bin.get_cell(1, 0));
+	EXPECT_EQ(0, bin.get_cell(1, 1));
+	EXPECT_EQ(true, bin.get_bit(1, 1));
+	EXPECT_EQ(false, bin.get_bit(1, 0));
+
 	EXPECT_FALSE(bin.row_vec(1).get_bit(0));
 	EXPECT_TRUE(bin.row_vec(1).get_cell(0));
 	EXPECT_TRUE(bin.row_vec(1).get_bit(1));
 	EXPECT_FALSE(bin.row_vec(1).get_cell(1));
-	
-	vec=bin.row_vec(1);
+
+	vec = bin.row_vec(1);
 	EXPECT_EQ(2, vec.get_cell(0));
 	EXPECT_EQ(0, vec.get_cell(1));
 	EXPECT_EQ(true, vec.get_bit(1));
 	EXPECT_EQ(false, vec.get_bit(0));
-	
-	vec=bin.row_vec(0);
+
+	vec = bin.row_vec(0);
 	EXPECT_EQ(1, vec.get_cell(0));
 	EXPECT_EQ(0, vec.get_cell(1));
 	EXPECT_EQ(true, vec.get_bit(0));
 	EXPECT_EQ(false, vec.get_bit(1));
-	
+
 	BinaryVector<uint8_t> vec_big(10);
 	BinaryVector<uint8_t> vec_small(7);
-	EXPECT_ANY_THROW(bin.write_vec(1,vec_big));
-	EXPECT_ANY_THROW(bin.write_vec(1,vec_small));
+	EXPECT_ANY_THROW(bin.write_vec(1, vec_big));
+	EXPECT_ANY_THROW(bin.write_vec(1, vec_small));
 }
 }
