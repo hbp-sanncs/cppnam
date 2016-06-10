2994792985bb70ca9ad5b4e57090916d54f483ff
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2016-06-10 11:28:08 +0200

    Embed test.json in unit test

--- test/util/test_read_json.cpp
+++ test/util/test_read_json.cpp
@@ -16,7 +16,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -26,10 +26,48 @@
 #include "core/spiking_parameters.hpp"
 
 namespace nam {
+
+static const std::string test_json =
+    "{\n"
+    "\t\"data\": {\n"
+    "\t\t\"n_bits_in\": 100,\n"
+    "\t\t\"n_bits_out\": 100,\n"
+    "\t\t\"n_ones_in\": 4,\n"
+    "\t\t\"n_ones_out\": 4,\n"
+    "\t\t\"n_samples\" : 1000\n"
+    "\t},\n"
+    "\n"
+    "\t\"network\": {\n"
+    "\t\t\"params\": {\n"
+    "\t\t\t\"e_rev_E\": 0.0,\n"
+    "\t\t\t\"v_rest\": -70.0,\n"
+    "\t\t\t\"v_reset\": -80.0,\n"
+    "\t\t\t\"v_thresh\": -57.0,\n"
+    "\t\t\t\"tau_syn_E\": 2.0,\n"
+    "\t\t\t\"tau_refrac\": 0.0,\n"
+    "\t\t\t\"tau_m\": 50.0,\n"
+    "\t\t\t\"cm\": 0.2\n"
+    "\t\t},\n"
+    "\t\t\"neuron_type\": \"IF_cond_exp\",\n"
+    "\t\t\"weight\": 0.01,\n"
+    "\t\t\"input_burst_size\": 1,\n"
+    "\t\t\"output_burst_size\": 1,\n"
+    "\t\t\"time_window\": 100.0,\n"
+    "\t\t\"isi\": 2.0,\n"
+    "\t\t\"sigma_t\": 2.0,\n"
+    "\t\t\"sigma_offs\": 0.0,\n"
+    "\t\t\"p0\": 0.0,\n"
+    "\t\t\"p1\": 0.0,\n"
+    "\t\t\"general_offset\" : 100\n"
+    "\t}\n"
+    "}\n"
+    "";
+
 TEST(ReadJSON, json_to_map)
 {
-	std::ifstream ifs("test.json", std::ifstream::in);
-	cypress::Json json(ifs);
+	std::stringstream ss(test_json);
+	cypress::Json json(ss);
+
 	auto map = json_to_map<float>(json["network"]);
 	EXPECT_EQ(map.end(), map.find("neuron_type"));
 	EXPECT_NEAR(0.01, map.find("weight")->second, 1e-8);
@@ -46,10 +84,10 @@ TEST(ReadJSON, json_to_map)
 
 TEST(ReadJSON, read_check)
 {
-	std::ifstream ifs("test.json", std::ifstream::in);
-	cypress::Json json(ifs);
-	auto map = json_to_map<float>(json["network"]);
+	std::stringstream ss(test_json);
+	cypress::Json json(ss);
 
+	auto map = json_to_map<float>(json["network"]);
 	auto res = read_check<float>(
 	    map, NetworkParameters::names,
 	    std::vector<float>(NetworkParameters::names.size(), 0));
