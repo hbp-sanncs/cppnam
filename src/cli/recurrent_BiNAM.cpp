9c3e8d2520601c3ae8ce0d05490335b8210925ab
Christoph Jenzen <cjenzen@techfak.uni-bielefeld.de>
2016-09-23 14:29:12 +0200

    small changes to recurrent binam

--- src/cli/recurrent_BiNAM.cpp
+++ src/cli/recurrent_BiNAM.cpp
@@ -17,6 +17,7 @@
  */
 
 #include <cstring>
+#include <thread>
 
 #include "core/binam.hpp"
 #include "core/entropy.hpp"
@@ -49,25 +50,46 @@ void information_graph(size_t bits_in, size_t bits_out, size_t ones_in,
                        size_t ones_out, size_t max_sample, bool rec = false)
 {
 	std::ofstream file;
-	file.open("binam_data.txt", std::ios::out);
+	file.open("binam_data.csv", std::ios::out);
 	file << "Samples, info, fp";
 	if (rec) {
-		file << ", info_rec, fp_rec";
+		file << ", info_rec, fp_rec, fn_rec";
+		file << ", info_rec_opti, fp_rec_opti, fn_rec_opti";
 	}
 	file << std::endl;
 	for (size_t i = 1; i <= max_sample; i++) {
 		DataParameters params(bits_in, bits_out, ones_in, ones_out, i);
 		BiNAM_Container<uint64_t> binam(params);
-		binam.set_up().recall();
-		auto se = binam.false_bits();
-		double info = entropy_hetero(params, se);
-		file << i << "," << info << ","
-		     << binam.sum_false_bits(binam.false_bits()).fp;
+		ExpResults se1, se2, se3;
+		std::thread normal_binam([&]() mutable {
+			binam.set_up().recall();
+			se1 = binam.analysis();
+		});
+		RecBinam binam2, binam3;
+
 		if (rec) {
-			RecBinam binam2(params);
-			binam2.set_up(true);
-			ExpResults se2 = binam2.analysis();
-			file << "," << se2.Info << "," << se2.fp;
+			binam2 = RecBinam(params);
+			binam3 = RecBinam(params);
+			std::thread true_rec_binam([&]() mutable {
+				binam2.set_up(true);
+				se2 = binam2.analysis();
+			});
+			std::thread false_rec_binam([&]() mutable {
+				binam3.set_up(false);
+				se3 = binam3.analysis();
+			});
+
+			true_rec_binam.join();
+			false_rec_binam.join();
+		}
+
+		normal_binam.join();
+
+		// double info = entropy_hetero(params, se1);
+		file << i << "," << se1.Info << "," << se1.fp;
+		if (rec) {
+			file << "," << se2.Info << "," << se2.fp << "," << se2.fn;
+			file << "," << se3.Info << "," << se3.fp << "," << se3.fn;
 		}
 		file << std::endl;
 		show_progress(double(i) / double(max_sample));
@@ -77,18 +99,19 @@ void information_graph(size_t bits_in, size_t bits_out, size_t ones_in,
 }
 int main(int argc, char *argv[])
 {
-	if (argc < 4 && argc >= 9) {
+	if (argc < 4 || argc >= 9) {
 		std::cerr
 		    << "Usage: ./recurrent_BiNAM <BITS> <ONES> <SAMPLES> (<RECURRENT>)"
-			<<std::endl
+		    << std::endl
 		    << "or <BITS_IN> <BITS_OUT> <ONES_IN> <ONES_OUT> <MAX_SAMPLES> "
 		       "(<RECURRENT>) (sweep)"
-			<<std::endl
+		    << std::endl
 		    << "or <BITS_IN> <BITS_OUT> <ONES_IN> <ONES_OUT> <SAMPLES> "
-		       "<Single> (<RECURRENT>)"
+		       "(<RECURRENT>)"
 		    << std::endl;
 		return 1;
 	}
+
 	bool rec = false;
 	for (int i = 1; i < argc; i++) {
 		if (strcmp(argv[i], "RECURRENT") == 0 ||
@@ -96,38 +119,41 @@ int main(int argc, char *argv[])
 			rec = true;
 		}
 	}
-	if ((argc == 6 && !rec) || (argc ==7 && rec)) {
+	if ((argc == 7 && !rec) || (argc == 8 && rec)) {
 		information_graph(std::stoi(argv[1]), std::stoi(argv[2]),
 		                  std::stoi(argv[3]), std::stoi(argv[4]),
 		                  std::stoi(argv[5]), rec);
 		return 0;
 	}
-
-	int n_bits = std::stoi(argv[1]);
-	int n_bits_out = std::stoi(argv[1]);
-	int n_ones = std::stoi(argv[2]);
-	int n_ones_out = std::stoi(argv[2]);
-	int n_samples = std::stoi(argv[3]);
-	if (argc == 8) {
-		n_bits = std::stoi(argv[1]);
-		n_bits_out = std::stoi(argv[2]);
-		n_ones = std::stoi(argv[3]);
-		n_ones_out = std::stoi(argv[4]);
-		n_samples = std::stoi(argv[5]);
-	}
-
-	std::cout << n_bits << " bits, " << n_ones << " ones and " << n_samples
-	          << " samples" << std::endl;
-	DataParameters params(n_bits, n_bits_out, n_ones, n_ones_out, n_samples);
-	if (rec) {
-		RecBinam binam(params);
-		auto res = binam.set_up().analysis();
-		res.print();
-	}
 	else {
-		BiNAM_Container<uint64_t> binam(params);
-		auto res = binam.set_up().recall().analysis();
-		res.print();
+
+		int n_bits = std::stoi(argv[1]);
+		int n_bits_out = std::stoi(argv[1]);
+		int n_ones = std::stoi(argv[2]);
+		int n_ones_out = std::stoi(argv[2]);
+		int n_samples = std::stoi(argv[3]);
+		if (argc > 5) {
+			n_bits = std::stoi(argv[1]);
+			n_bits_out = std::stoi(argv[2]);
+			n_ones = std::stoi(argv[3]);
+			n_ones_out = std::stoi(argv[4]);
+			n_samples = std::stoi(argv[5]);
+		}
+
+		std::cout << n_bits << " bits, " << n_ones << " ones and " << n_samples
+		          << " samples" << std::endl;
+		DataParameters params(n_bits, n_bits_out, n_ones, n_ones_out,
+		                      n_samples);
+		if (rec) {
+			RecBinam binam(params);
+			auto res = binam.set_up(false).analysis();
+			res.print();
+		}
+		else {
+			BiNAM_Container<uint64_t> binam(params);
+			auto res = binam.set_up().recall().analysis();
+			res.print();
+		}
 	}
 	return 0;
 }
